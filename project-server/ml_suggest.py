047b0c1cff6d58d04ad9a6fb70d661b488dbdcdd
James Zhang <zyz991016@outlook.com>
2023-08-23 17:49:29 -0700

    Update ml_suggest.py
    change model to ann

--- project-server/ml_suggest.py
+++ project-server/ml_suggest.py
@@ -1,5 +1,5 @@
 import pandas
-from sklearn.linear_model import LogisticRegression
+from sklearn.neural_network import MLPClassifier
 import pickle
 
 data = pandas.read_csv("data.csv")
@@ -8,21 +8,31 @@ X = data[['soil_hum', 'air_temp', 'air_hum', 'future_temp', 'future_hum']]
 Y = data['water']
 Z = data['roof']
 
-# multinomial logistic regression
-water_logreg = LogisticRegression(max_iter=10000, C=2)
-water_logreg.fit(X.values,Y.values)
+# ANN configuration
+hidden_layer_sizes = (20, 20)  # Two hidden layers with 20 neurons each
+activation = 'relu'
+solver = 'adam'
+max_iter = 10000
+alpha = 1e-4
 
-roof_logreg = LogisticRegression(max_iter=10000, C=2)
-roof_logreg.fit(X.values,Z.values)
+# Create and train the ANNs
+water_ann = MLPClassifier(hidden_layer_sizes=hidden_layer_sizes, activation=activation, 
+                          solver=solver, alpha=alpha, max_iter=max_iter)
+water_ann.fit(X.values, Y.values)
+
+roof_ann = MLPClassifier(hidden_layer_sizes=hidden_layer_sizes, activation=activation, 
+                         solver=solver, alpha=alpha, max_iter=max_iter)
+roof_ann.fit(X.values, Z.values)
 
 """
-water_suggest = water_logreg.predict([[38.2,21.5,35.1,12.5,42.2]])
-roof_suggest = roof_logreg.predict([[38.2,21.5,35.1,12.5,42.2]])
+water_suggest = water_ann.predict([[38.2, 21.5, 35.1, 12.5, 42.2]])
+roof_suggest = roof_ann.predict([[38.2, 21.5, 35.1, 12.5, 42.2]])
 """
 
-filename1 = "water_suggest.pickle"
-filename2 = "roof_suggest.pickle"
+filename1 = "water_suggest_ann.pickle"
+filename2 = "roof_suggest_ann.pickle"
 
-# save model
-pickle.dump(water_logreg, open(filename1, "wb"))
-pickle.dump(roof_logreg, open(filename2, "wb"))
+# Save models
+pickle.dump(water_ann, open(filename1, "wb"))
+pickle.dump(roof_ann, open(filename2, "wb"))
+
